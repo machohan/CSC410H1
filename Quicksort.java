f7b086030a541a23ab8e31e93fde39493356b48c
Ammar <Ammar-chohan@hotmail.com>
2016-10-21 18:33:13 -0400

    Updated from Ammar and Samir

--- Quicksort.java
+++ Quicksort.java
@@ -1,92 +1,95 @@
-public class IntervalDriver {
+public class Quicksort {
 
-    // Do not change or add annotations in this class!
-    //@ public invariant this != null;
-    
-    /*@ requires i1 != null;
-      @ requires i2 != null;
-      @ ensures \result != null;
-      @ ensures \result.low == \result.high
-      @         || (\result.low == Math.min(i1.low, i2.low) 
-      @         && \result.high == Math.max(i1.high, i2.high));
+    // Write formal pre- and post-conditions for this method.
+    /*@ assignable \nothing;
+      @ requires a != null;
+      @ requires a.length >= 0;
+      @ modifies a[*];
+      @ ensures \old(a.length) == a.length;
+      @ ensures (\forall int i; i >= 0 && i < a.length; (\exists int j; j >=0 &&
+	j < a.length; \old(a[i]) == a[j]));
+      @ ensures (\forall int i; i>=0 && i<(a.length-1); a[i] <= a[i+1] );
+      @ ensures (\sum int i; i >= 0 && i < a.length; a[i]) ==
+	(\sum int j; j >= 0 && j < \old(a.length); \old(a[j]));
       @*/
-    OpenInterval joinIntervals(OpenInterval i1, OpenInterval i2) {
-	// Returns empty interval if i1 and i2 are non-overlapping.
-	if (i1.getHigh() < i2.getLow() || i2.getHigh() < i1.getLow()) {
-	    return new OpenInterval(0);
-	}
+    public static void sort(int[] a, int ulimit, int llimit)
+    {
+        quicksort(a, 0, a.length, ulimit, llimit);
+    }
+	
+    /*@ assignable \nothing;
+      @ requires a != null;
+      @ requires start >= 0;
+      @ requires stop >= 0;
+      @ requires start <= a.length;
+      @ requires stop <= a.length;
+      @ modifies a[*];
+      @ ensures \old(a.length) == a.length;
+      @*/
+    private static void quicksort(int[] a, int start, int stop, int ulimit, int llimit)
+    {
+        if (stop - start > 1) {
+            int p = pivot(a, start, stop, ulimit, llimit);
+            quicksort(a, start, p, a[p], llimit);
+            quicksort(a, p+1, stop, ulimit, a[p]);
+        }
+    }
+	
+    /*@ assignable \nothing;
+      @ requires a != null;
+      @ requires start >= 0;
+      @ requires stop - start > 1;
+      @ requires start <= a.length;
+      @ requires stop <= a.length;
+      @ modifies a[*];
+      @ ensures \old(a.length) == a.length;
+      @ ensures \result >= 0;
+      @ ensures \result <= stop - 1;
+      @*/
+    private static int pivot(int[] a, int start, int stop, int ulimit, int llimit)
+    {
+        int p = partition(a, a[start], start+1, stop, ulimit, llimit);
+        if (start < p)
+            swap(a, start, p);
+        return p;
+    }
+	
+    /*@ assignable \nothing;
+      @ requires a != null;
+      @ requires start >= 0;
+      @ requires stop >= 0;
+      @ requires start <= stop;
+      @ requires stop <= a.length;
+      @ modifies a[*];
+      @ ensures \old(a.length) == a.length;
+      @ ensures \result >= start - 1;
+      @ ensures \result <= stop - 1;
+      @ */
+    private static int partition(int[] a, int pivot, int start, int stop, int ulimit, int llimit)
+    {
+        if (start >= stop) 
+            return start - 1;
+        if (a[start] < pivot)
+            return partition(a, pivot, start + 1, stop, ulimit, llimit);
+        if (a[--stop] > pivot)
+            return partition(a, pivot, start, stop, ulimit, llimit);
+        if (start < stop) {
+            swap(a, start, stop);
+            return partition(a, pivot, start+1, stop, ulimit, llimit);
+        } else
+            return start;
+    }
 
-	// Joins i1 and i2.
-	int low = i1.getLow() < i2.getLow() ? i1.getLow() : i2.getLow();
-	int high = i1.getHigh() > i2.getHigh() ? i1.getHigh() : i2.getHigh();
-	return new OpenInterval(low, high);
+    /*@ requires a != null;
+      @ requires 0 <= i && i < a.length;
+      @ requires 0 <= j && j < a.length;
+      @ modifies a[i], a[j];
+      @ ensures a[i] == \old(a[j]) && a[j] == \old(a[i]);
+      @*/
+    public static void swap(int[] a, int i, int j)
+    {
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
     }
 }
-
-class OpenInterval{
-
-    // Do not change implementations in this class!
-    private /*@ spec_public @*/ int low;
-    private /*@ spec_public @*/ int high;
-
-    // Do not change this invariant!
-    //@ invariant low <= high;
-
-    // Creates an non-empty interval.
-    // ADD CONTRACTS #TODO
-    /*@ requires low <= high;
-      @ ensures this.low <= this.high;
-      @ ensures this.low == low;
-      @ ensures this.high == high;
-      @*/
-    public OpenInterval(int low, int high){
-	this.low = low;
-	this.high = high;
-    }
-
-    // Creates an empty interval.
-    //ADD CONTRACTS #TODO
-    /*@ ensures this.low == x;
-      @ ensures this.high == x;
-      @*/
-    public OpenInterval(int x){
-	this.low = x;
-	this.high = x;
-    }	
-     
-    // Returns lower bound.
-    //ADD CONTRACTS #TODO
-    /*@ requires this != null;
-      @ ensures \result == this.low;
-      @*/
-    public int getLow(){
-	return this.low;
-    }
-
-    // Returns upper bound.
-    //ADD CONTRACTS #TODO
-    /*@ requires this != null;
-      @ ensures \result == this.high;
-      @*/
-    public int getHigh(){
-	return this.high;
-    }
-
-    /*@ requires x != null;
-      @ requires this != null;
-      @ ensures (this.low == x.low && this.high == x.high) ==> \result;
-      @*/
-    public /*@ pure */ boolean equals(OpenInterval x){
-	return (this.low == x.low && this.high == x.high);
-    }
-}
-
-
-
-
-
-
-
-
-
-
